277bf852578505c0f93df30c8e8f7c13c3f2eb24
yosemmmmmm <yosemmmmmm@github.com>
2026-03-30 13:25:17 +0800

    后端启动优化

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,8 +1,8 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppModule } from '../app.module';
+import { AppService } from '../app.service';
 import { User, UserSchema } from '../users/schemas/user.schema';
 import { AuthService } from './auth.service';
 import { PasswordService, TokenService } from './auth.helpers';
@@ -10,7 +10,6 @@ import { AuthSession, AuthSessionSchema } from './schemas/auth-session.schema';
 
 @Module({
   imports: [
-    forwardRef(() => AppModule),
     ConfigModule,
     JwtModule.registerAsync({
       inject: [ConfigService],
@@ -23,7 +22,7 @@ import { AuthSession, AuthSessionSchema } from './schemas/auth-session.schema';
       { name: AuthSession.name, schema: AuthSessionSchema },
     ]),
   ],
-  providers: [AuthService, PasswordService, TokenService],
+  providers: [AuthService, PasswordService, TokenService, AppService],
   exports: [AuthService, PasswordService, TokenService],
 })
 export class AuthModule {}
